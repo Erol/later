984c9e8888391c5d2afdbaa7c3023384c27a7af6
Erol Fornoles <erol.fornoles@gmail.com>
2012-09-28 12:18:05 +0800

    Added API documentation.

--- lib/later.rb
+++ lib/later.rb
@@ -18,33 +18,73 @@ module Later
       end
     end
 
+    # Get the Nest key of the schedule.
+    #
+    #   Later[:reservations].key #=> Later::reservations
+
     def key
       @key
     end
+
+    # Get the Nest key of the schedule exception list.
+    #
+    #   Later[:reservations].exceptions #=> Later::reservations::exceptions
 
     def exceptions
       @exceptions ||= key[:exceptions]
     end
 
+    # Get the schedule time of a unique event.
+    #
+    #   Later[:reservations].set 'event-1', Time.parse('2012-09-28 11:36:17 +0800')
+    #   Later[:reservations]['event-1'] #=> 2012-09-28 11:36:17 +0800
+
     def [](event)
       Time.at key[:schedule].zscore(event) rescue nil
     end
+
+    # Gets the number of unique scheduled events.
+    #
+    #   Later[:reservations].set 'event-1', Time.now + 60
+    #   Later[:reservations].set 'event-2', Time.now + 120
+    #   Later[:reservations].set 'event-3', Time.now + 180
+    #   Later[:reservations].count #=> 3
 
     def count
       key[:schedule].zcard
     end
 
+    # Set a unique event to the schedule.
+    #
+    #   Later[:reservations].set 'event-1', Time.now + 60
+
     def set(event, time)
       key[:schedule].zadd time.to_i, event
     end
+
+    # Unset a unique event from the schedule.
+    #
+    #   Later[:reservations].unset 'event-2'
 
     def unset(event)
       key[:schedule].zrem event
     end
 
+    # When called inside an `each` block, `stop!` signals the block to halt processing of the schedule.
+    #
+    #   Later[:reservations].each do |event|
+    #     Later[:reservations].stop!
+    #   end
+
     def stop!
       @stop = true
     end
+
+    # Process each event on the schedule. The block only gets called when an event is due to run based on the current time.
+    #
+    #   Later[:reservations].each do |event|
+    #     # Do something with the event
+    #   end
 
     def each(&block)
       @stop = false
