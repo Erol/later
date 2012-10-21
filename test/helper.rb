32a9f8d8e04ccbff61caa1ecb854c96894ee6c56
Erol Fornoles <erol.fornoles@gmail.com>
2012-10-21 14:51:27 +0800

    No need to spawn a new Redis server instance for every test.

--- test/helper.rb
+++ test/helper.rb
@@ -1,17 +1,3 @@
 require 'bundler/setup'
 require 'later'
 require 'minitest/autorun'
-
-class MiniTest::Unit::TestCase
-  def setup
-    system 'redis-server', File.join(File.dirname(__FILE__), 'redis.conf')
-    until File.exist? 'redis.pid'
-      sleep 0.1
-    end
-  end
-
-  def teardown
-    system "kill `cat redis.pid`"
-    sleep 0.1
-  end
-end
